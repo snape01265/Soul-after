06cf7f2cdcb2e8f27acd33618d145d58e50395a4
snape01265 <swl6507@gmail.com>
2021-08-04 00:59:55 +0900

    abcd

--- Soul_After_/Assets/Scripts/Dialogue Scripts/DeactivateTalk.cs
+++ Soul_After_/Assets/Scripts/Dialogue Scripts/DeactivateTalk.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 //RPG Talk Holder 게임 오브젝트를 Activate or Deactivate 하는 스크립트
 public class DeactivateTalk : MonoBehaviour
 {
+    public string previousCutscene;
+    public string watchedCutscene;
+    public CutsceneList cutsceneList;
+
+    private void Update()
+    {
+        if (!cutsceneList.initialValue.Contains(watchedCutscene))
+        {
+            if (cutsceneList.initialValue.Contains(previousCutscene))
+            {
+                GetComponent<Collider2D>().enabled = true;
+            }
+            else
+            {
+                GetComponent<Collider2D>().enabled = false;
+            }
+        }
+        else
+        {
+            Deactivate();
+        }
+    }
     public void Activate()
     {
         gameObject.SetActive(true);
