2725e7725ccf1173cc8da6464195891dec985ba3
cgm-16 <crazy4u5@naver.com>
2021-08-06 00:45:33 +0900

    puzzle WIP

--- Soul_After_/Assets/Scripts/ButtonRenderer.cs
+++ Soul_After_/Assets/Scripts/ButtonRenderer.cs
@@ -5,21 +5,73 @@ using UnityEngine;
 
 public class ButtonRenderer : MonoBehaviour
 {
-    public BoolList ButtonStates;
+    public BoolList buttonStates;
+    private GameObject BtnUp;
+    private bool BtnState;
+    private int BtnIdx;
 
     void Start()
     {
-        char a = this.gameObject.ToString()[this.gameObject.ToString().Length - 1];
-        bool btnstate = ButtonStates.initialValue[int.Parse(a.ToString())-1];
-        if (btnstate)
+        if (!buttonStates.initialValue.Equals(buttonStates.defaultValue))
+        {
+            buttonStates.initialValue = new List<bool>(buttonStates.defaultValue);
+        }
+
+        Debug.Log("this is =" + this.gameObject.name);
+        char a = this.gameObject.name[this.gameObject.name.Length - 1];
+        Debug.Log("char is =" + a.ToString());
+        BtnIdx = int.Parse(a.ToString()) - 1;
+        BtnState = buttonStates.initialValue[BtnIdx];
+
+        if (BtnState)
         {
             // if true, disable Button Up sprite
+            BtnUp = this.transform.Find("Button Up").gameObject;
+            BtnUp.SetActive(false);
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         // disable Button Up Sprite
         // also change ButtonState accordingly
+
+        BtnUp = this.transform.Find("Button Up").gameObject;
+
+        if (BtnUp.activeSelf && collision.GetComponent<Collider2D>().CompareTag($"MovableObject"))
+        {
+            Debug.Log(this.name + "is pressed!");
+            Debug.Log("BtnIdx = " + BtnIdx);
+            BtnUp.SetActive(false);
+            buttonStates.initialValue[BtnIdx] = true;
+        }
+
+        if (this.gameObject.CompareTag($"ResetBtn") && collision.GetComponent<Collider2D>().CompareTag($"Player"))
+        {
+            Debug.Log("Reset");
+            buttonStates.initialValue = new List<bool>(buttonStates.defaultValue);
+            InitBtn();
+        }
+    }
+
+    private void InitBtn()
+    {
+        BtnUp = this.transform.Find("Button Up").gameObject;
+        BtnState = buttonStates.initialValue[BtnIdx];
+
+        if (BtnState)
+        {
+            BtnUp.SetActive(false);
+        } else
+        {
+            BtnUp.SetActive(true);
+        }
+    }
+
+    public void ButtonUp()
+    {
+        BtnUp = this.transform.Find("Button Up").gameObject;
+        BtnUp.SetActive(true);
+        buttonStates.initialValue[BtnIdx] = false;
     }
 }
