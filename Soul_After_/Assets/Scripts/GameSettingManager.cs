b4073bb9781f770d9562b2f7f9d16c1abc31846d
cgm-16 <crazy4u5@naver.com>
2021-08-12 02:15:25 +0900

    menu

--- Soul_After_/Assets/Scripts/GameSettingManager.cs
+++ Soul_After_/Assets/Scripts/GameSettingManager.cs
@@ -3,20 +3,13 @@ using UnityEngine.UI;
 
 public class GameSettingManager : MonoBehaviour
 {
-    private void Awake()
-    {
-        bool isFS = Screen.fullScreenMode == FullScreenMode.ExclusiveFullScreen;
-        string curTxt = isFS ? "창 모드" : "전체화면" ;
-        this.transform.Find("Text").GetComponent<Text>().text = curTxt;
-    }
-
     public void ToggleFullscreen()
     {
         bool isFS = Screen.fullScreenMode == FullScreenMode.ExclusiveFullScreen;
         string curTxt = isFS ?  "창 모드" : "전체화면";
 
         Screen.fullScreenMode = isFS ? FullScreenMode.Windowed : FullScreenMode.ExclusiveFullScreen;
-        this.transform.Find("Text").GetComponent<Text>().text = curTxt;
+        this.transform.Find("Fullscreen Mode/Text").GetComponent<Text>().text = curTxt;
     }
 
     public void AdjustVol(float volLvl)
