50292ab492a4f242d38ca5eed0623b8f133e90d6
cgm-16 <crazy4u5@naver.com>
2021-08-19 02:18:12 +0900

    saveslotsystem

--- Soul_After_/Assets/Scripts/Save Data/GameSaveManager.cs
+++ Soul_After_/Assets/Scripts/Save Data/GameSaveManager.cs
@@ -73,11 +73,11 @@ public class GameSaveManager : MonoBehaviour
             string results = "";
             foreach (ScriptableObject scriptable in objToSave)
             {
-                results += JsonUtility.ToJson(scriptable) + " ";
+                results += JsonUtility.ToJson(scriptable) + "_";
                 Debug.Log(JsonUtility.ToJson(scriptable));
                 Debug.Log("results is " + results);
             }
-            
+
             binary.Serialize(file, results);
 
             file.Close();
@@ -101,7 +101,7 @@ public class GameSaveManager : MonoBehaviour
                 FileStream file = File.Open(Application.persistentDataPath +
                     string.Format("/Save{0}.dat", slotNo), FileMode.Open);
                 BinaryFormatter binary = new BinaryFormatter();
-                string[] results = binary.Deserialize(file).ToString().Split(' ');
+                string[] results = ((string)binary.Deserialize(file)).Split('_');
                 for (int i = 0; i < objToSave.Count; i++)
                 {
                     JsonUtility.FromJsonOverwrite(results[i], objToSave[i]);
@@ -114,6 +114,67 @@ public class GameSaveManager : MonoBehaviour
         {
             Debug.LogWarning(e.StackTrace);
             return false;
+        }
+    }
+
+    // 시간상으로 최근 데이터 로드
+    public void LoadRecent()
+    {
+        List<DateTime> dateTimes = new List<DateTime>();
+        int slotNo = 0;
+        int curTimeIdx = 18;
+
+        for (int n = 0; n < 3; n++)
+        {
+            try
+            {
+                if (File.Exists(Application.persistentDataPath +
+                string.Format("/Save{0}.dat", n)))
+                {
+                    FileStream file = File.Open(Application.persistentDataPath +
+                        string.Format("/Save{0}.dat", n), FileMode.Open);
+                    BinaryFormatter binary = new BinaryFormatter();
+                    string[] results = ((string)binary.Deserialize(file)).Split('_');
+                    StringValue saveTime = ScriptableObject.CreateInstance(typeof(StringValue)) as StringValue;
+                    JsonUtility.FromJsonOverwrite(results[curTimeIdx], saveTime);
+                    dateTimes.Add(DateTime.Parse(saveTime.initialValue));
+                    file.Close();
+                }
+                else dateTimes.Add(DateTime.MinValue);
+            }
+            catch (Exception e)
+            {
+                Debug.LogWarning(e.StackTrace);
+            }
+        }
+
+        for (int i = 1; i < 2; i++)
+        {
+            if (DateTime.Compare(dateTimes[slotNo], dateTimes[i]) > 0) 
+            {
+                slotNo = i;
+            }
+        }
+
+        try
+        {
+            if (File.Exists(Application.persistentDataPath +
+            string.Format("/Save{0}.dat", slotNo)))
+            {
+                FileStream file = File.Open(Application.persistentDataPath +
+                    string.Format("/Save{0}.dat", slotNo), FileMode.Open);
+                BinaryFormatter binary = new BinaryFormatter();
+                string[] results = ((string)binary.Deserialize(file)).Split('_');
+                for (int i = 0; i < objToSave.Count; i++)
+                {
+                    JsonUtility.FromJsonOverwrite(results[i], objToSave[i]);
+                }
+                file.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning(e.StackTrace);
         }
     }
 }
--- Soul_After_/Assets/Scripts/Save Data/SavePoint.cs
+++ Soul_After_/Assets/Scripts/Save Data/SavePoint.cs
@@ -1,10 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class SavePoint : MonoBehaviour
 {
     public VectorValue startingPosition;
+    public StringValue curTime;
 
     private bool inRange;
     
@@ -13,6 +13,7 @@ public class SavePoint : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && inRange)
         {
             startingPosition.initialValue = new Vector2(transform.position.x, transform.position.y - 1);
+            curTime.initialValue = DateTime.Now.ToString();
         }
     }
     private void OnTriggerEnter2D(Collider2D other)
