8830e5d3b10adfeaba203bad3e70f7febdbf8e4f
snape01265 <swl6507@gmail.com>
2021-08-25 04:50:14 +0900

    abcdefghijklm

--- Soul_After_/Assets/Scripts/Battle System/PlayerHealth.cs
+++ Soul_After_/Assets/Scripts/Battle System/PlayerHealth.cs
@@ -11,11 +11,15 @@ public class PlayerHealth : MonoBehaviour
     public int needSP;
     public bool levelClear;
     public GameObject SPDisplay;
-    private Text text;
-    private CameraShake shake;
     public AudioSource hitSound;
     public AudioSource acqSound;
+    public RPGTalk rpgTalk;
+
     private Gameover gameOver;
+    private Text text;
+    private CameraShake shake;
+    private IEnumerator currentIFrame;
+    private IEnumerator currentDamageConvo;
 
     [Header("Invulnerability Frame")]
     public Color flashColor;
@@ -37,12 +41,27 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage(int dmg)
     {
         HP -= dmg;
-        if(HP <= 0)
+        if (HP <= 0)
         {
             Death();
             gameOver.EndGame();
         }
-        StartCoroutine(IFrame());
+        if (currentIFrame != null)
+        {
+        }
+        else
+        {
+            StartCoroutine(IFrame());
+            currentIFrame = IFrame();
+        }
+        if (currentDamageConvo != null)
+        {
+        }
+        else
+        {
+            StartCoroutine(DamageConvo());
+            currentDamageConvo = DamageConvo();
+        }
     }
 
     public void AcquireSoul(int soul)
@@ -68,7 +87,7 @@ public class PlayerHealth : MonoBehaviour
         triggerCollider.enabled = false;
         hitSound.Play();
         shake.CamShakeWithImage();
-        while(temp < numberOfFlashes)
+        while (temp < numberOfFlashes)
         {
             mySprite.color = flashColor;
             yield return new WaitForSeconds(flashDuration);
@@ -76,6 +95,29 @@ public class PlayerHealth : MonoBehaviour
             yield return new WaitForSeconds(flashDuration);
             temp++;
         }
+        currentIFrame = null;
         triggerCollider.enabled = true;
+    }
+    IEnumerator DamageConvo()
+    {
+        int rand = Random.Range(0, 3);
+        if (rand == 0)
+        {
+            rpgTalk.NewTalk("hit1_start", "hit1_end", rpgTalk.txtToParse);
+        }
+        else if (rand == 1)
+        {
+            rpgTalk.NewTalk("hit2_start", "hit2_end", rpgTalk.txtToParse);
+        }
+        else if (rand == 2)
+        {
+            rpgTalk.NewTalk("hit3_start", "hit3_end", rpgTalk.txtToParse);
+        }
+        else if (rand == 3)
+        {
+            rpgTalk.NewTalk("hit4_start", "hit4_end", rpgTalk.txtToParse);
+        }
+        yield return new WaitForSeconds(3.5f);
+        currentDamageConvo = null;
     }
 }
