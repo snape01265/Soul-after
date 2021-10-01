62c233767ea6a04d713e4890bc9903f9d37df410
outofthepark101 <outofthepark101@gmail.com>
2021-10-01 21:07:53 +0900

    asd

--- Soul_After_/Assets/Scripts/PuzzleController.cs
+++ Soul_After_/Assets/Scripts/PuzzleController.cs
@@ -6,11 +6,11 @@ public class PuzzleController : MonoBehaviour
 {
     [SerializeField] private Transform emptySpace;
     [SerializeField] private TileRenderer[] tiles;
+    private TileRenderer tileRenderer;
     private int emptySpaceIndex = 11;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -22,7 +22,7 @@ public class PuzzleController : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
             if (hit.collider != null)
             {
-                if (Vector2.Distance(emptySpace.position, hit.transform.position) < 2) //Change the distance value when receive the painting
+                if (Vector2.Distance(emptySpace.position, hit.transform.position) < 4) //Change the distance value when receive the painting
                 {
                     TileRenderer thisTile = hit.transform.GetComponent<TileRenderer>();
                     Vector2 lastEmptySpacePosition = emptySpace.position;
@@ -66,5 +66,10 @@ public class PuzzleController : MonoBehaviour
             }
         }
         return -1;
+    }
+    public void ResetPuzzle()
+    {
+        tileRenderer = GameObject.FindGameObjectWithTag("TileId").GetComponent<TileRenderer>();
+        tileRenderer.targetPosition = tileRenderer.startPosition;
     }
 }
--- Soul_After_/Assets/Scripts/TileRenderer.cs
+++ Soul_After_/Assets/Scripts/TileRenderer.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class TileRenderer : MonoBehaviour
 {
-    public int tileId;
     public Vector3 targetPosition;
     [SerializeField] private Vector3 correctPosition;
+    public Vector3 startPosition;
     public bool inRightPlace;
     private SpriteRenderer _sprite;
     private void Awake()
     {
+        startPosition = transform.position;
         targetPosition = transform.position;
         _sprite = GetComponent<SpriteRenderer>();
     }
