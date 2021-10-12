e93d2fc0aec93d0fcd97bd9e680e2f582a879645
outofthepark101 <outofthepark101@gmail.com>
2021-10-13 00:29:10 +0900

    Revert "as"
    This reverts commit 8fe084abb213d50499d6fec6df3c9f3dfa16df4d.

--- Soul_After_/Assets/Scripts/RaceBehavior.cs
+++ Soul_After_/Assets/Scripts/RaceBehavior.cs
@@ -121,7 +121,7 @@ public class RaceBehavior : MonoBehaviour
             timer.text = countDown.ToString();
             countDown -= 1;
             //SFX should play when count down begins. But audio source restarts for some reasong (debugging needed)
-            yield return new WaitForSeconds(1.22f);
+            yield return new WaitForSeconds(1);
         }
         timer.text = "GO!";
         //After "Go!" is shown on the screen, the text should disappear and the bgm should play. 
