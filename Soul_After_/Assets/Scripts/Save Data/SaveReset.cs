e098660b08eb609c0fea9fa8bee9c14abfe4b9b0
Gwangmin Choi <crazy4u5@naver.com>
2021-11-08 19:36:12 +0900

    savereset fix

--- Soul_After_/Assets/Scripts/Save Data/SaveReset.cs
+++ Soul_After_/Assets/Scripts/Save Data/SaveReset.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Linq;
 using UnityEngine;
 
 // 사용되는 Scene: Start Menu
@@ -38,12 +39,6 @@ public class SaveReset : MonoBehaviour
                         ob.initialValue = ob.defaultValue;
                         break;
                     }
-                case "VectorList":
-                    {
-                        VectorList ob = (VectorList)obj;
-                        ob.defaultPos = new List<Vector2>();
-                        break;
-                    }
                 case "BoolValue":
                     {
                         BoolValue ob = (BoolValue)obj;
@@ -54,7 +49,8 @@ public class SaveReset : MonoBehaviour
                     {
                         BoolList ob = (BoolList)obj;
                         ob.initialValue = new List<bool>();
-                        ob.defaultValue = new List<bool>();
+                        foreach (bool b in ob.defaultValue)
+                            ob.initialValue.Add(b);
                         break;
                     }
                 case "AnimatorValue":
@@ -67,7 +63,8 @@ public class SaveReset : MonoBehaviour
                     {
                         CutsceneList ob = (CutsceneList)obj;
                         ob.initialValue = new List<string>();
-                        ob.defaultValue = new List<string>();
+                        foreach (string s in ob.defaultValue)
+                            ob.initialValue.Add(s);
                         break;
                     }
                 case "FloatValue":
