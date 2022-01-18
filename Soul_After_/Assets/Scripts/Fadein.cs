a017062162908d249b1af3ec14ea3c290da19e90
Gwangmin Choi <crazy4u5@naver.com>
2022-01-19 01:32:00 +0900

    fadein.cs fixes

--- Soul_After_/Assets/Scripts/Fadein.cs
+++ Soul_After_/Assets/Scripts/Fadein.cs
@@ -7,8 +7,13 @@ public class Fadein : MonoBehaviour
     public GameObject fadeInPanel;
     public GameObject fadeOutPanel;
 
+    private Transform player;
+    private Vector3 pos = Vector3.zero;
+
     private void Awake()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
         if (fadeInPanel != null)
         {
             GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
@@ -21,6 +26,16 @@ public class Fadein : MonoBehaviour
         StartCoroutine(FIO(Time));
     }
 
+    public void SetPosX(float XPos)
+    {
+        pos = new Vector3(XPos, pos.y, pos.z);
+    }
+
+    public void SetPosY(float YPos)
+    {
+        pos = new Vector3(pos.x, YPos, pos.z);
+    }
+
     private IEnumerator FIO(float time)
     {
         if (fadeOutPanel != null)
@@ -29,6 +44,7 @@ public class Fadein : MonoBehaviour
             Destroy(panel, 1);
             yield return new WaitForSeconds(time);
         }
+        player.position = pos;
         if (fadeInPanel != null)
         {
             GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity);
