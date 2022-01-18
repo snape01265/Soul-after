8c7229dd7c3938879c6fa81bf8ceb1e2f435667a
Gwangmin Choi <crazy4u5@naver.com>
2022-01-19 00:52:39 +0900

    fadein.cs - FadeInOut() 추가

--- Soul_After_/Assets/Scripts/Fadein.cs
+++ Soul_After_/Assets/Scripts/Fadein.cs
@@ -5,12 +5,33 @@ using UnityEngine;
 public class Fadein : MonoBehaviour
 {
     public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
 
     private void Awake()
     {
         if (fadeInPanel != null)
         {
             GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);
+        }
+    }
+
+    public void FadeInOut(float Time)
+    {
+        StartCoroutine(FIO(Time));
+    }
+
+    private IEnumerator FIO(float time)
+    {
+        if (fadeOutPanel != null)
+        {
+            GameObject panel = Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity);
+            Destroy(panel, 1);
+            yield return new WaitForSeconds(time);
+        }
+        if (fadeInPanel != null)
+        {
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity);
             Destroy(panel, 1);
         }
     }
