06c7994e64e4a0b62541cd788e071300eb0cc02a
Gwangmin Choi <crazy4u5@naver.com>
2022-01-28 02:39:49 +0900

    boxpuzzle scripts

---
+++ Soul_After_/Assets/Scripts/PushBox.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushBox : MonoBehaviour
+{
+    public Transform playerPos;
+    
+    [HideInInspector]
+    // 1 : UP, 2 : RIGHT, 3 : LEFT, 4 : DOWN 
+    public int PushedPos = 0;
+    [HideInInspector]
+    public Vector3 LastLoc;
+    [HideInInspector]
+    public bool teled = false;
+
+    private GameObject DestCalcNode;
+    private bool boxTouched = false;
+    private Vector2 touchedPoint;
+    private Vector3 targetPos;
+
+    void Start()
+    {
+        targetPos = transform.position;
+        DestCalcNode = transform.Find("DestCalcNode").gameObject;
+        LastLoc = transform.position;
+    }
+
+    private void Update()
+    {
+        if (boxTouched && Input.GetButtonDown("Jump"))
+        {
+            if (touchedPoint.y == 1)
+            {
+                PushedPos = 1;
+                DestCalcUp();
+            } else if (touchedPoint.x == 1)
+            {
+                PushedPos = 2;
+                DestCalcRight();
+            } else if (touchedPoint.x == -1)
+            {
+                PushedPos = 3;
+                DestCalcLeft();
+            } else
+            {
+                PushedPos = 4;
+                DestCalcDown();
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        while (Vector3.Distance(targetPos, transform.position) > .1f)
+            transform.position = Vector3.Lerp(transform.position, targetPos, 0.05f);
+        transform.position = targetPos;
+    }
+
+    public void DestCalcUp()
+    {
+        DestCalcNode.transform.position = transform.position + Vector3.up;
+        while (true)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(DestCalcNode.transform.position, DestCalcNode.transform.forward);
+            if (hit.collider != null && !hit.transform.gameObject.GetComponent<PortalActive>() && hit.transform.gameObject.GetComponent<IceTile>())
+            {
+                DestCalcNode.transform.position += Vector3.up;
+            }
+            else
+            {
+                PushToDest(DestCalcNode.transform.position);
+                return;
+            }
+        }
+    }
+
+    public void DestCalcDown()
+    {
+        DestCalcNode.transform.position = transform.position + Vector3.down;
+        while (true)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(DestCalcNode.transform.position, DestCalcNode.transform.forward);
+            if (hit.collider != null && !hit.transform.gameObject.GetComponent<PortalActive>() && hit.transform.gameObject.GetComponent<IceTile>())
+            {
+                DestCalcNode.transform.position += Vector3.down;
+            }
+            else
+            {
+                PushToDest(DestCalcNode.transform.position);
+                return;
+            }
+        }
+    }
+
+    public void DestCalcRight()
+    {
+        DestCalcNode.transform.position = transform.position + Vector3.right;
+        while (true)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(DestCalcNode.transform.position, DestCalcNode.transform.forward);
+            if (hit.collider != null && !hit.transform.gameObject.GetComponent<PortalActive>() && hit.transform.gameObject.GetComponent<IceTile>())
+            {
+                DestCalcNode.transform.position += Vector3.right;
+            }
+            else
+            {
+                PushToDest(DestCalcNode.transform.position);
+                return;
+            }
+        }
+    }
+
+    public void DestCalcLeft()
+    {
+        DestCalcNode.transform.position = transform.position + Vector3.left;
+        while (true)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(DestCalcNode.transform.position, DestCalcNode.transform.forward);
+            if (hit.collider != null && !hit.transform.gameObject.GetComponent<PortalActive>() && hit.transform.gameObject.GetComponent<IceTile>())
+            {
+                DestCalcNode.transform.position += Vector3.left;
+            }
+            else
+            {
+                PushToDest(DestCalcNode.transform.position);
+                return;
+            }
+        }
+    }
+
+    public void PushToDest(Vector3 Dest)
+    {
+        targetPos = Dest;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            boxTouched = true;
+            touchedPoint = collision.GetContact(0).normal;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        boxTouched = false;
+    }
+}
