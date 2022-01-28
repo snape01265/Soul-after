bce8151c2de1a7f1df38d85e958c06df212d83df
Gwangmin Choi <crazy4u5@naver.com>
2022-01-29 00:23:42 +0900

    added portalactive

--- Soul_After_/Assets/Scripts/PortalActive.cs
+++ Soul_After_/Assets/Scripts/PortalActive.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PortalActive : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform PortalBuddy;
+    private PushBox pushBox;
+
+    private void Start()
     {
-        
+        pushBox = GameObject.Find("PushBox").GetComponent<PushBox>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("PushBox"))
+        {
+            Tele();
+        }
+    }
+
+    private void Tele()
+    {
+        pushBox.teled = true;
+        pushBox.targetPos = PortalBuddy.transform.position;
+        pushBox.transform.position = PortalBuddy.position;
+        int pushPos = 5 - pushBox.PushedPos;
+        switch (pushPos)
+        {
+            case 1:
+                pushBox.DestCalcUp();
+                break;
+            case 2:
+                pushBox.DestCalcRight();
+                break;
+            case 3:
+                pushBox.DestCalcLeft();
+                break;
+            case 4:
+                pushBox.DestCalcDown();
+                break;
+        }
     }
 }
--- Soul_After_/Assets/Scripts/PushBoxPuzzleManager.cs
+++ Soul_After_/Assets/Scripts/PushBoxPuzzleManager.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 
 public class PushBoxPuzzleManager : MonoBehaviour
 {
-
+    public GameObject[] PortalPairs;
 }
