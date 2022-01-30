d03107040800c9bc83ba2ae81ecf2d0394edab39
snape01265 <swl6507@gmail.com>
2022-01-31 03:26:56 +0900

    reset button

--- Soul_After_/Assets/Scripts/Battle System/Box Puzzle/PushBoxPuzzleManager.cs
+++ Soul_After_/Assets/Scripts/Battle System/Box Puzzle/PushBoxPuzzleManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PushBoxPuzzleManager : MonoBehaviour
 {
     public KeyCode keyForMirror;
+    public KeyCode keyForReset;
     public int turnCount;
     public int puzzleNum;
     public bool goalReached = false;
@@ -38,7 +39,7 @@ public class PushBoxPuzzleManager : MonoBehaviour
                 MirrorActivate(isMirrorWorld);
             }
         }
-        if (turnCount == 0)
+        else if (Input.GetKeyDown(keyForReset) && isAvailable && !box.pushing)
         {
             switch (puzzleNum)
             {
@@ -53,6 +54,22 @@ public class PushBoxPuzzleManager : MonoBehaviour
                     break;
             }
         }
+        else if (turnCount == 0)
+        {
+            switch (puzzleNum)
+            {
+                case 1:
+                    StartCoroutine(Reset(12));
+                    break;
+                case 2:
+                    StartCoroutine(Reset(9));
+                    break;
+                case 3:
+                    StartCoroutine(Reset(2));
+                    break;
+            }
+        }
+
         else if (turnCount >= 0 && goalReached && !box.pushing)
         {
             switch (puzzleNum)
