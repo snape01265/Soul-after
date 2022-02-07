a4380574a909b1a9442369e57ba870e878422ef5
Gwangmin Choi <crazy4u5@naver.com>
2022-02-08 01:10:13 +0900

    minor fixes

--- Soul_After_/Assets/Scripts/Battle System/Ep3_Dungeon/Ep3_Dungeon_Phase_3_P1.cs
+++ Soul_After_/Assets/Scripts/Battle System/Ep3_Dungeon/Ep3_Dungeon_Phase_3_P1.cs
@@ -23,20 +23,13 @@ public class Ep3_Dungeon_Phase_3_P1 : MonoBehaviour
 
     IEnumerator ColOfFloors()
     {
-        bool first = true;
-
         while (true)
         {
-            if (first)
-                first = !first;
-            else
+            for (int i = LavaFloors.Length - 1; i >= 0; i--)
             {
                 if (sfx)
                     sfx.Play();
-            }
 
-            for (int i = LavaFloors.Length - 1; i >= 0; i--)
-            {
                 for (int j = 0; j < LavaFloors.Length; j++)
                 {
                     if (j != i)
