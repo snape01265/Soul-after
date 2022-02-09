51e59f4b0f06d37fd85053784d27949b1b3c23a1
Gwangmin Choi <crazy4u5@naver.com>
2022-02-10 00:43:48 +0900

    oops

--- Soul_After_/Assets/Scripts/Battle System/Box Puzzle/PortalCircuitSwitch.cs
+++ Soul_After_/Assets/Scripts/Battle System/Box Puzzle/PortalCircuitSwitch.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class PortalCircuitSwitch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] PortalPairs;
+
+    private Animator anim;
+    private bool pressed = false;
+    private Collider2D playerCol;
+    private bool isStartingPair = true;
+
+    private void Awake()
     {
-        
+        anim = GetComponent<Animator>();
+        playerCol = GameObject.Find("Player").GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetButtonDown("Jump") && gameObject.GetComponent<Collider2D>().IsTouching(playerCol))
+        {
+            pressed = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.GetComponent<Collider2D>().CompareTag("Player"))
+        {
+            if (pressed)
+            {
+                pressed = false;
+                CircuitChange();
+                if (anim.GetBool("Flicked") == false)
+                    anim.SetBool("Flicked", true);
+                else if (anim.GetBool("Flicked") == true)
+                    anim.SetBool("Flicked", false);
+            }
+        }
+    }
+
+    private void CircuitChange()
+    {
+        if (isStartingPair)
+        {
+            isStartingPair = false;
+            PortalPairs[0].SetActive(false);
+            PortalPairs[1].SetActive(false);
+            PortalPairs[2].SetActive(true);
+            PortalPairs[3].SetActive(true);
+
+        }
+        else
+        {
+            isStartingPair = true;
+            PortalPairs[0].SetActive(true);
+            PortalPairs[1].SetActive(true);
+            PortalPairs[2].SetActive(false);
+            PortalPairs[3].SetActive(false);
+        }
     }
 }
+
