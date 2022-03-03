d0f06ca41deba6e9bb51b47be6a767e0b1315d73
Gwangmin Choi <crazy4u5@naver.com>
2022-03-04 02:23:45 +0900

    added invis puzzle elmements

---
+++ Soul_After_/Assets/Scripts/Battle System/Ep.5_Puzzles/Invismaze_Goal.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invismaze_Goal : MonoBehaviour
+{
+    public InvismazePuzzleManager PuzzleManager;
+
+    private bool _isGoal = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !_isGoal)
+        {
+            _isGoal = true;
+            PuzzleManager.FinPuzzle();
+            StartCoroutine(FlipGoal());
+        }
+    }
+
+    IEnumerator FlipGoal()
+    {
+        yield return new WaitForSeconds(.1f);
+        _isGoal = false;
+    }
+}
