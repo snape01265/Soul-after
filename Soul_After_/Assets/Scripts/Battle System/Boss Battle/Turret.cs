fa969dedc60e5ca8b534cbc2ea0dc626e098d20a
snape01265 <swl6507@gmail.com>
2022-03-12 00:14:57 +0900

    turret camera control

--- Soul_After_/Assets/Scripts/Battle System/Boss Battle/Turret.cs
+++ Soul_After_/Assets/Scripts/Battle System/Boss Battle/Turret.cs
@@ -1,33 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Turret : MonoBehaviour
 {
     [HideInInspector]
-    public bool playerInRange = false;
+    public CinemachineVirtualCamera playerCam;
+    [HideInInspector]
+    public CinemachineVirtualCamera turretCam;
 
+    private bool playerInRange = false;
     private GameObject playerPosition;
-    private bool onTurret;
+    private bool onTurret = false;
     private Player player;
+    private Animator playerAnim;
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         playerPosition = GameObject.FindGameObjectWithTag("Player");
+        playerAnim = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F) && !onTurret)
+        if (Input.GetKeyDown(KeyCode.F) && !onTurret && playerInRange)
         {
             Vector3 turretPosition = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
             playerPosition.transform.position = turretPosition;
             player.CancelControl();
+            playerAnim.SetFloat("Move_X", 0);
+            playerAnim.SetFloat("Move_Y", 1);
+            playerAnim.SetBool("Moving", false);
             onTurret = true;
+            playerCam.Priority = 0;
+            turretCam.Priority = 1;
         }
         else if (Input.GetKeyDown(KeyCode.F) && onTurret)
         {
             player.GiveBackControl();
+            playerCam.Priority = 1;
+            turretCam.Priority = 0;
+            onTurret = false;
+        }
+    }
+    private void OnDisable()
+    {
+        if(onTurret)
+        {
+            player.GiveBackControl();
+            playerCam.Priority = 1;
+            turretCam.Priority = 0;
             onTurret = false;
         }
     }
