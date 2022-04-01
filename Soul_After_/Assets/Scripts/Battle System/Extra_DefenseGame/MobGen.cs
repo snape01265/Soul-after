134f3a868e8a893852c8fb694e2d8f89f40265ca
Gwangmin Choi <crazy4u5@naver.com>
2022-04-02 01:30:15 +0900

    파일 위치 변경

---
+++ Soul_After_/Assets/Scripts/Battle System/Extra_DefenseGame/MobGen.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobGen : MonoBehaviour
+{
+    public DefenseGameManager gameManager;
+
+    public void SpawnMob()
+    {
+
+    }
+}
