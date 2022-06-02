e2d48d16f82dd8a05feb87f054837f7df04acc4b
Gwangmin Choi <crazy4u5@naver.com>
2022-06-03 00:48:30 +0900

    added checkcontinue.cs to continue button

---
+++ Soul_After_/Assets/Scripts/CheckContinue.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckContinue : MonoBehaviour
+{
+    private readonly int CURTIMEIDX = 17;
+
+    void Start()
+    {
+        for (int n = 0; n < 3; n++)
+        {
+            try
+            {
+                if (File.Exists(Application.persistentDataPath +
+                string.Format("/Save{0}.dat", n)))
+                {
+                    GetComponent<Button>().interactable = true;
+                    return;
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogWarning(e.StackTrace);
+            }
+        }
+        
+    }
+}
