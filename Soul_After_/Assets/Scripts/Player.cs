495695318dc71aec7943bdc07b6395a36687f1f4
outofthepark101 <outofthepark101@gmail.com>
2022-08-08 00:01:19 +0900

    bug fixes up to ep.1

--- Soul_After_/Assets/Scripts/Player.cs
+++ Soul_After_/Assets/Scripts/Player.cs
@@ -29,7 +29,7 @@ public class Player : MonoBehaviour
     [NonSerialized]
     public bool control = true;
     [NonSerialized]
-    public float speed = 9;
+    public float speed = 8;
 
     private Rigidbody2D myRigidbody;
     private Vector3 change;
