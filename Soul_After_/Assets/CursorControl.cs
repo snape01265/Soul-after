0b518a8af24ef8aa25e7f2182ba0d16d1ebe257c
outofthepark101 <outofthepark101@gmail.com>
2022-08-12 15:56:35 +0900

    Cpr fixed and Cursor added

---
+++ Soul_After_/Assets/CursorControl.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorControl : MonoBehaviour
+{
+    public Texture2D cursorArrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+}
