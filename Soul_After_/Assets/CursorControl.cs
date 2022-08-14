24423b05e8e477f3f3d13d80d643516c1280a339
outofthepark101 <outofthepark101@gmail.com>
2022-08-14 17:45:12 +0900

    Scene transition triggers for ep.1 deleteed

--- Soul_After_/Assets/CursorControl.cs
+++ Soul_After_/Assets/CursorControl.cs
@@ -6,9 +6,12 @@ public class CursorControl : MonoBehaviour
 {
     public Texture2D cursorArrow;
 
+    private Vector2 cursorHotspot;
+
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+        cursorHotspot = new Vector2(cursorArrow.width / 2.8f, cursorArrow.height / 2.8f);
+        Cursor.SetCursor(cursorArrow, cursorHotspot, CursorMode.ForceSoftware);
     }
 }
